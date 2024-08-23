3be5ab86f7101703075c002c6251882a9c1b467e
maggiechen <chenmaggiejob@gmail.com>
2024-08-23 08:30:41 +0800

    修正回首頁會換行的問題

--- web10/index.php
+++ web10/index.php
@@ -18,8 +18,9 @@
 	<iframe name="back" style="display:none;"></iframe>
 	<div id="all">
 		<div id="title">
-			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
-		<a href="index.php" style="float:right">回首頁</a>
+			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36
+			<a href="index.php" style="float:right">回首頁</a>
+		</div>
 		<div id="title2">
 			<a href="index.php">
 				<img src="./icon/02B01.jpg" alt="">
